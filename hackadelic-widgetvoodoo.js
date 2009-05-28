acfa362e00e840841cdc6bbe1aa5d6d4c9dc44b8
Hackadelic <Hackadelic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-28 18:55:43 +0000

    tagging 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/hackadelic-widgetvoodoo/tags/1.0.6@121167 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hackadelic-widgetvoodoo.js
@@ -0,0 +1,5 @@
+;String.prototype.format=function(){var s=this;for(i=0;i<arguments.length;i++)
+s=s.replace(new RegExp('\\{'+i+'\\}','gi'),arguments[i]);return s;}
+jQuery(document).ready(function(){var ws=WidgetVoodooSelectors.widget;var ts=WidgetVoodooSelectors.title;var cs=WidgetVoodooSelectors.autocollapse;var widgets=jQuery(ws);widgets.each(function(index,elem){var self=jQuery(this);var title=jQuery(ts,self);if(!title.length||!title.html().replace('&nbsp;',''))return true;var body=jQuery(':not('+ts+')',self);var wid='widgetbody-'+index;if(body.length>1){var wrap=jQuery('<DIV class="widgetbody {0}">{1}</DIV>'.format(wid,self.html()));title=jQuery(ts,wrap);self.html(wrap);title.insertBefore(wrap);}
+else
+body.addClass('widgetbody '+wid);title.addClass('collapsible');self.addClass('collapsible');title.click(function(){jQuery(ws+' .'+wid).slideToggle('fast');jQuery(this).toggleClass('collapsed');});});if(cs){jQuery.each(cs.split(/\s*,\s*/),function(i,it){jQuery(it+' .widgetbody').hide();jQuery(it+' '+ts).addClass('collapsed');});}});
